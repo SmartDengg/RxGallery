ebb7499f7e6fcb7d2d630c7c204b59467c8be371
小鄧子 <Hi4Joker@gmail.com>
2016-04-09 17:27:46 +0800

    替换icon

--- app/src/main/java/com/smartdengg/smartgallery/domain/GalleryUseCase.java
+++ app/src/main/java/com/smartdengg/smartgallery/domain/GalleryUseCase.java
@@ -22,127 +22,138 @@ import rx.subscriptions.Subscriptions;
  */
 public class GalleryUseCase extends UseCase<List<FolderEntity>> {
 
-  private final String[] GALLERY_PROJECTION = {
-      MediaStore.Images.Media.DATA, MediaStore.Images.Media.DISPLAY_NAME, MediaStore.Images.Media.DATE_ADDED,
-      MediaStore.Images.Media._ID
-  };
+    private final String[] GALLERY_PROJECTION = {
+            MediaStore.Images.Media.DATA, MediaStore.Images.Media.DISPLAY_NAME, MediaStore.Images.Media.DATE_ADDED,
+            MediaStore.Images.Media._ID
+    };
 
-  private final CursorLoader cursorLoader;
-  private List<FolderEntity> items = new ArrayList<>();
+    private final CursorLoader cursorLoader;
+    private List<FolderEntity> items = new ArrayList<>();
 
-  private FolderEntity folderEntity = new FolderEntity();
-  private ImageEntity imageEntity = new ImageEntity();
+    private FolderEntity folderEntity = new FolderEntity();
+    private ImageEntity imageEntity = new ImageEntity();
 
-  private GalleryUseCase(Context context) {
-    this.cursorLoader =
-        new CursorLoader(context, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, GALLERY_PROJECTION, null, null,
-                         GALLERY_PROJECTION[2] + " DESC");
-  }
+    private GalleryUseCase(Context context) {
+        this.cursorLoader = new CursorLoader(context,
+                                             MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                             GALLERY_PROJECTION,
+                                             null,
+                                             null,
+                                             GALLERY_PROJECTION[2] + " DESC");
+    }
 
-  public static GalleryUseCase createdUseCase(Context context) {
-    return new GalleryUseCase(context);
-  }
+    public static GalleryUseCase createdUseCase(Context context) {
+        return new GalleryUseCase(context);
+    }
 
-  @Override protected Observable<List<FolderEntity>> interactor() {
+    @Override
+    protected Observable<List<FolderEntity>> interactor() {
 
-    return Observable.create(new Observable.OnSubscribe<Cursor>() {
-      @Override public void call(final Subscriber<? super Cursor> subscriber) {
+        return Observable.create(new Observable.OnSubscribe<Cursor>() {
+            @Override
+            public void call(final Subscriber<? super Cursor> subscriber) {
 
-        final Cursor cursor = cursorLoader.loadInBackground();
+                final Cursor cursor = cursorLoader.loadInBackground();
 
-        subscriber.add(Subscriptions.create(new Action0() {
-          @Override public void call() {
-            if (cursorLoader.isStarted() && cursor != null && !cursor.isClosed()) {
-              cursorLoader.cancelLoad();
-              cursor.close();
+                subscriber.add(Subscriptions.create(new Action0() {
+                    @Override
+                    public void call() {
+                        if (cursorLoader.isStarted() && cursor != null && !cursor.isClosed()) {
+                            cursorLoader.cancelLoad();
+                            cursor.close();
+                        }
+                    }
+                }));
+
+                // TODO: 2016/3/28  cursor作为事件被发送 ????
+                if (cursor == null) {
+                    Observable.error(new NullPointerException("cursor must not be null"));
+                } else {
+                    try {
+                        while (cursor.moveToNext()) {
+                            if (cursor.getString(cursor.getColumnIndexOrThrow(GALLERY_PROJECTION[0]))
+                                      .endsWith(".gif")) {
+                                continue;
+                            }
+
+                            if (cursor.isClosed() || subscriber.isUnsubscribed()) break;
+                            subscriber.onNext(cursor);
+                        }
+                    } catch (Exception e) {
+                        if (!subscriber.isUnsubscribed()) Observable.error(e);
+                    } finally {
+                        if (!cursor.isClosed()) cursor.close();
+                        if (!subscriber.isUnsubscribed()) subscriber.onCompleted();
+                    }
+                }
             }
-          }
-        }));
+        }).map(new Func1<Cursor, ImageEntity>() {
+            @Override
+            public ImageEntity call(Cursor cursor) {
 
-        // TODO: 2016/3/28  cursor作为事件被发送 ????
-        if (cursor == null) {
-          Observable.error(new NullPointerException("cursor must not be null"));
-        } else {
-          try {
-            while (cursor.moveToNext()) {
-              if (cursor.getString(cursor.getColumnIndexOrThrow(GALLERY_PROJECTION[0])).endsWith(".gif")) {
-                continue;
-              }
+                String imagePath = cursor.getString(cursor.getColumnIndexOrThrow(GALLERY_PROJECTION[0]));
+                String imageName = cursor.getString(cursor.getColumnIndexOrThrow(GALLERY_PROJECTION[1]));
+                long addDate = cursor.getLong(cursor.getColumnIndexOrThrow(GALLERY_PROJECTION[2]));
 
-              if (cursor.isClosed() || subscriber.isUnsubscribed()) break;
-              subscriber.onNext(cursor);
+                ImageEntity clone = imageEntity.newInstance();
+
+                clone.setImagePath(imagePath);
+                clone.setImageName(imageName);
+                clone.setDate(addDate);
+
+                return clone;
             }
-          } catch (Exception e) {
-            if (!subscriber.isUnsubscribed()) Observable.error(e);
-          } finally {
+        }).filter(new Func1<ImageEntity, Boolean>() {
+            @Override
+            public Boolean call(ImageEntity imageEntity) {
 
-            if (!cursor.isClosed()) cursor.close();
-            if (!subscriber.isUnsubscribed()) subscriber.onCompleted();
-          }
-        }
-      }
-    }).map(new Func1<Cursor, ImageEntity>() {
-      @Override public ImageEntity call(Cursor cursor) {
+                /*校验文件是否存在*/
+                File file = new File(imageEntity.getImagePath());
+                File parentFile = file.getParentFile();
 
-        String imagePath = cursor.getString(cursor.getColumnIndexOrThrow(GALLERY_PROJECTION[0]));
-        String imageName = cursor.getString(cursor.getColumnIndexOrThrow(GALLERY_PROJECTION[1]));
-        long addDate = cursor.getLong(cursor.getColumnIndexOrThrow(GALLERY_PROJECTION[2]));
+                return file.exists() && parentFile != null;
+            }
+        }).doOnNext(new Action1<ImageEntity>() {
+            @Override
+            public void call(ImageEntity imageEntity) {
 
-        ImageEntity clone = imageEntity.newInstance();
+                File folderFile = new File(imageEntity.getImagePath()).getParentFile();
+                FolderEntity clone = folderEntity.newInstance();
+                clone.setFolderName(folderFile.getName());
+                clone.setFolderPath(folderFile.getAbsolutePath());
 
-        clone.setImagePath(imagePath);
-        clone.setImageName(imageName);
-        clone.setDate(addDate);
+                if (!items.contains(clone)) {
+                    clone.setThumbPath(imageEntity.getImagePath());
+                    clone.addImage(imageEntity);
+                    items.add(clone);
+                } else {
+                    items.get(items.indexOf(clone)).addImage(imageEntity);
+                }
+            }
+        }).toList().map(new Func1<List<ImageEntity>, FolderEntity>() {
+            @Override
+            public FolderEntity call(List<ImageEntity> imageEntities) {
 
-        return clone;
-      }
-    }).filter(new Func1<ImageEntity, Boolean>() {
-      @Override public Boolean call(ImageEntity imageEntity) {
+                FolderEntity clone = folderEntity.newInstance();
+                clone.setFolderName("所有图片");
+                clone.setFolderPath("");
+                clone.setChecked(true);
+                clone.setThumbPath(imageEntities.get(0).getImagePath());
+                clone.setImageEntities(imageEntities);
 
-        /*校验文件是否存在*/
-        File file = new File(imageEntity.getImagePath());
-        File parentFile = file.getParentFile();
+                return clone;
+            }
+        }).map(new Func1<FolderEntity, List<FolderEntity>>() {
+            @Override
+            public List<FolderEntity> call(FolderEntity folderEntity) {
 
-        return file.exists() && parentFile != null;
-      }
-    }).doOnNext(new Action1<ImageEntity>() {
-      @Override public void call(ImageEntity imageEntity) {
+                List<FolderEntity> galleryFolderEntities = new ArrayList<>(items.size() + 1);
 
-        File folderFile = new File(imageEntity.getImagePath()).getParentFile();
-        FolderEntity clone = folderEntity.newInstance();
-        clone.setFolderName(folderFile.getName());
-        clone.setFolderPath(folderFile.getAbsolutePath());
+                galleryFolderEntities.add(folderEntity);
+                galleryFolderEntities.addAll(items);
 
-        if (!items.contains(clone)) {
-          clone.setThumbPath(imageEntity.getImagePath());
-          clone.addImage(imageEntity);
-          items.add(clone);
-        } else {
-          items.get(items.indexOf(clone)).addImage(imageEntity);
-        }
-      }
-    }).toList().map(new Func1<List<ImageEntity>, FolderEntity>() {
-      @Override public FolderEntity call(List<ImageEntity> imageEntities) {
-
-        FolderEntity clone = folderEntity.newInstance();
-        clone.setFolderName("所有图片");
-        clone.setFolderPath("");
-        clone.setChecked(true);
-        clone.setThumbPath(imageEntities.get(0).getImagePath());
-        clone.setImageEntities(imageEntities);
-
-        return clone;
-      }
-    }).map(new Func1<FolderEntity, List<FolderEntity>>() {
-      @Override public List<FolderEntity> call(FolderEntity folderEntity) {
-
-        List<FolderEntity> galleryFolderEntities = new ArrayList<>(items.size() + 1);
-
-        galleryFolderEntities.add(folderEntity);
-        galleryFolderEntities.addAll(items);
-
-        return galleryFolderEntities;
-      }
-    }).compose(SchedulersCompat.<List<FolderEntity>>applyIoSchedulers());
-  }
+                return galleryFolderEntities;
+            }
+        }).compose(SchedulersCompat.<List<FolderEntity>>applyIoSchedulers());
+    }
 }
