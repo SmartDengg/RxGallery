1aa4eb749026ceff1da6982f8e288ca9dfd811a8
SmartDengg <dengwei@lianjia.com>
2016-05-17 18:25:14 +0800

    remove unused code

--- app/src/main/java/com/smartdengg/smartgallery/MyApplication.java
+++ app/src/main/java/com/smartdengg/smartgallery/MyApplication.java
@@ -1,16 +1,11 @@
 package com.smartdengg.smartgallery;
 
-import android.app.ActivityManager;
 import android.app.Application;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import com.orhanobut.logger.LogLevel;
 import com.orhanobut.logger.Logger;
 import com.squareup.picasso.Picasso;
-
-import static android.content.pm.ApplicationInfo.FLAG_LARGE_HEAP;
-import static android.os.Build.VERSION.SDK_INT;
-import static android.os.Build.VERSION_CODES.HONEYCOMB;
 
 /**
  * *   ┏┓　　　┏┓
@@ -51,24 +46,9 @@ public class MyApplication extends Application {
 
         Picasso.setSingletonInstance(picasso);
 
-        Logger.init("SmartDengg")
+        Logger.init(this.getPackageName())
               .setMethodOffset(0)
               .setMethodCount(4)
               .setLogLevel(LogLevel.FULL);
-
-        Logger.t(0)
-              .d(calculateMemoryCacheSize() + "");
     }
-
-    int calculateMemoryCacheSize() {
-        ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-        boolean largeHeap = (this.getApplicationInfo().flags & FLAG_LARGE_HEAP) != 0;
-        int memoryClass = am.getMemoryClass();
-        if (largeHeap && SDK_INT >= HONEYCOMB) {
-            memoryClass = am.getLargeMemoryClass();
-        }
-        // Target ~15% of the available heap.
-        return memoryClass / 7;
-    }
-
 }
