bf94682ba84bed9b9ee6975cf6bb1fec3a904fd7
小鄧子 <Hi4Joker@gmail.com>
2016-08-22 01:11:21 +0800

    add processBuilder

--- app/src/main/java/com/smartdengg/rxgallery/example/activity/MainActivity.java
+++ app/src/main/java/com/smartdengg/rxgallery/example/activity/MainActivity.java
@@ -3,11 +3,18 @@ package com.smartdengg.rxgallery.example.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.smartdengg.rxgallery.example.R;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,6 +24,23 @@ public class MainActivity extends AppCompatActivity {
 
   @NonNull @OnClick(R.id.gallery_button) protected void onGalleryClick() {
     GalleryActivity.navigateToGallery(MainActivity.this);
+
+    ProcessBuilder processBuilder =
+        new ProcessBuilder("adb shell am start -W com.smartdengg.aopexapmle/.MainActivity");
+    try {
+      Process process = processBuilder.start();
+      InputStream inputStream = process.getInputStream();
+      BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+      StringBuilder buffer = new StringBuilder();
+      String line = "";
+      while ((line = in.readLine()) != null) {
+        buffer.append(line);
+      }
+      String string = buffer.toString();
+      Log.d(TAG, string);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override protected void onDestroy() {
